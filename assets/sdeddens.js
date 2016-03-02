ae61dadcba0a95debcd54b86189ece20aae13eb0
Stephen Deddens <sdeddens@gmail.com>
2016-03-02 06:23:21 -0600

    completed contacts panel

--- assets/sdeddens.js
+++ assets/sdeddens.js
@@ -1,3 +1,7 @@
+// This code is retreaved and exacuted after the jQuary js files are loaded.
+// It is designed to defeat trolls from obtaining the email and phone number.
+// This is not really a concern.  It is included mostly to show that it can be done.
+
 var m = 'sdeddens';
 m += '@gm';
 m += 'ail';
@@ -5,10 +9,11 @@ m += '.com';
 var tgtElmt = $('#myMail');
 tgtElmt.attr('href', 'mailto:' + m);
 tgtElmt.html('Email: ' + m);
+
 m =  '+1-281';
 m += '-300-2630';
 tgtElmt = $('#myMobile');
 tgtElmt.attr('href', 'tel:' + m);
 m =  '+1-281-';
-m += '300-2630';
+m += '300-3630';
 tgtElmt.html('Mobile: ' + m);
